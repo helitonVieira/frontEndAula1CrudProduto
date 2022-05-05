4453b9be2d1205a0e8a6e7a28cdb322a05d09ec2
heliton Vieira <helitondba@gmail.com>
2022-05-04 21:28:04 -0300

    html comunicando com ts login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Credenciais } from './../../models/credenciais';
 import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
@@ -8,7 +9,12 @@ import { Router } from '@angular/router'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+
+  credenciais : Credenciais = {
+    email:'',
+    senha:''
+  }
+
   constructor(private router: Router, private headerService: HeaderService) {
     headerService.headerData = {
       title: 'Tela Login',
