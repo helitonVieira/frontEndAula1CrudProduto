a317b8cd85f2b57873e2053bcfc455363ed11d4b
heliton Vieira <helitondba@gmail.com>
2022-06-21 17:15:08 -0300

    prevenda delete

--- src/app/components/pre-venda/pre-venda-delete/pre-venda-delete.component.ts
+++ src/app/components/pre-venda/pre-venda-delete/pre-venda-delete.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PrevendaService } from './../prevenda.service';
+import { Prevenda } from './../../../models/Prevenda.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreVendaDeleteComponent implements OnInit {
 
-  constructor() { }
+  prevenda: Prevenda;
+
+  constructor(
+    private prevendaService: PrevendaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.prevendaService.readById(id).subscribe((prevenda) => {
+      this.prevenda = prevenda;
+    });
+  }
+
+  deleteSubcategoria(): void {
+    this.prevendaService.delete(this.prevenda.id).subscribe(() => {
+      this.prevendaService.showMessage("Prevenda excluido com sucesso!");
+      this.router.navigate(["/prevendas"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/prevendas"]);
   }
 
 }
